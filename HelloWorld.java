5f58459eed98183aea2e48b38140cdf706c4896f
beatrice2000 <126506911+beatrice2000@users.noreply.github.com>
2024-10-31 16:40:41 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world! & welcome jenkins");
+        System.out.println("Made some changes");
     }
 }
